4fb72463e722a2093bde52530440737d09980e94
Vidish Appadoo <krishnaappadoo99@gmail.com>
2024-09-25 11:31:55 +0400

    trying to display content of collections

--- express.js
+++ express.js
@@ -16,17 +16,40 @@ async function connectToMongo() {
   try {
     await client.connect();
     db = client.db('CST3144_M00953760');  
-    lessonsCollection = db.collection('lessons');
+    lessonsCollection = db.collection('LESSON');
+    ordersCollection = db.collection('ORDERS');
     console.log('Connected to MongoDB');
+    await logLessonsContent();
+    await logOrdersContent();
   } catch (error) {
     console.error('MongoDB connection failed:', error);
   }
 }
 
+// Function to log the contents of the lessons collection
+async function logLessonsContent() {
+    try {
+      const lessons = await lessonsCollection.find({}).toArray();
+      console.log('Lessons content:', lessons);
+    } catch (error) {
+      console.error('Failed to fetch lessons content:', error);
+    }
+  }
+
+  // Function to log the contents of the lessons collection
+async function logOrdersContent() {
+    try {
+      const orders = await ordersCollection.find({}).toArray();
+      console.log('Orders content:', orders);
+    } catch (error) {
+      console.error('Failed to fetch lessons content:', error);
+    }
+  }
+
 connectToMongo();
 
 // Start the server
-const PORT = 8000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
