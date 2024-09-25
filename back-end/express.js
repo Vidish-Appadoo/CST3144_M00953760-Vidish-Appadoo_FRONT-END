3c6d62c023ef70618c9410f883593c89c13c2f0c
Vidish Appadoo <krishnaappadoo99@gmail.com>
2024-09-25 20:53:25 +0400

    send fetched lessons from mongoDB to front-end

--- back-end/express.js
+++ back-end/express.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
 
 const MONGO_URI = "mongodb+srv://admin:admin@cluster0.opsc7.mongodb.net/"
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
+app.use(cors());
 
 // Connect to Mongo
 const client = new MongoClient(MONGO_URI);
@@ -19,37 +22,25 @@ async function connectToMongo() {
     lessonsCollection = db.collection('LESSON');
     ordersCollection = db.collection('ORDERS');
     console.log('Connected to MongoDB');
-    await logLessonsContent();
-    await logOrdersContent();
   } catch (error) {
     console.error('MongoDB connection failed:', error);
   }
 }
 
-// Function to log the contents of the lessons collection
-async function logLessonsContent() {
-    try {
-      const lessons = await lessonsCollection.find({}).toArray();
-      console.log('Lessons content:', lessons);
-    } catch (error) {
-      console.error('Failed to fetch lessons content:', error);
-    }
+  // API route to get lessons data
+app.get('/api/lessons', async (req, res) => {
+  try {
+    const lessons = await lessonsCollection.find({}).toArray();
+    res.json(lessons); // Send lessons data as JSON
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch lessons data' });
   }
-
-  // Function to log the contents of the lessons collection
-async function logOrdersContent() {
-    try {
-      const orders = await ordersCollection.find({}).toArray();
-      console.log('Orders content:', orders);
-    } catch (error) {
-      console.error('Failed to fetch lessons content:', error);
-    }
-  }
+});
 
 connectToMongo();
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
